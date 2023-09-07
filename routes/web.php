7867521c75b9783d3c7c2e6accaae05257707d17
David R <davidrami2000@gmail.com>
2023-09-07 14:13:16 +0200

    Adding routes

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,24 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// add index too
+
 Route::get('/about', function () {
     return view('about');
+});
+
+Route::get('/rooms', function () {
+    return view('rooms');
+});
+
+Route::get('/room-details', function () {
+    return view('room-details');
+});
+
+Route::get('/offers', function () {
+    return view('offers');
+});
+
+Route::get('/contact', function () { // should be POST ?
+    return view('contact');
 });
